7d914db679a82f6cf248091975ebefd2ac41735f
Helen <fer.helen@icloud.com>
2023-09-04 19:13:11 +0100

    change background color and img with js

--- desafios/desafio002/script.js
+++ desafios/desafio002/script.js
@@ -1,19 +1,24 @@
 function carregar(){
     var msg = window.document.getElementById("msg")
-    var img = window.document.getElementById("img")
+    var img = window.document.getElementById("imagem")
     var data = new Date()
-    var hora = data.getHours()
+    var hora = data.getHours() 
     msg.innerHTML = `Agora são ${hora} horas.`
 
-    if(hora >= 0 && hora < 12) {
-        //BOM DIA 
-        img.src = "bomdia.jpeg"
+    if (hora >= 0 && hora < 12) {
+        img.src = "assets/bomdia.jpeg"
+        document.body.style.background = "#e2cd9f"
     }
-} else if(hora >= 12 && < 18) {
-    //BOA TARDE
-    img.src = "boatarde.jpeg"
-}
-else {
-    //BOA NOITE
-    img.src = "boanoite.avif"
-}
+    else if (hora >=12 && hora <= 18) {
+        img.src = "assets/boatarde.jpeg" 
+        document.body.style.background = "#b5733a"
+    }
+    else {
+        img.src = "assets/boanoite.jpeg"
+        document.body.style.background = "#1868b8"
+    }
+
+} 
+
+
+
