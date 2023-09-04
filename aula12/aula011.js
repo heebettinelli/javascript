b1ebd6d2b02a0f1d99858f82d9ea85c4c84bd6ee
Helen <fer.helen@icloud.com>
2023-09-04 19:13:10 +0100

    condicoes aninhadas

---
+++ aula12/aula011.js
@@ -0,0 +1,12 @@
+var idade = 19
+if (idade < 16) {
+    console.log("Não vota")
+}
+else{
+    if(idade <=16 && idade < 17) {
+        console.log("Voto opcional")
+    }
+    if(idade >= 18){
+        console.log("Voto Obrigatório")
+    }
+}
