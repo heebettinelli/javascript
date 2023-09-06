82502ac6b4ca1ec517b3f89702aa11be5ec0906f
Helen <fer.helen@icloud.com>
2023-09-06 16:49:57 +0100

    while, do while, for

---
+++ aula13/ambiente.js
@@ -0,0 +1,19 @@
+//while exemple
+var c = 1
+while(c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+// dowhile exemple
+ var c = 1
+
+do{
+    console.log(`passo ${c}`)
+} while(c<= 6)
+
+
+// for exemple
+ for (c=1; c<= 6 ; c++) {
+     console.log(c) 
+}
