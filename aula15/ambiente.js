644fee5c054a3bf28c795635c64719c444bb1ffa
Helen <fer.helen@icloud.com>
2023-09-11 21:12:33 +0100

    learn array

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,3 +1,14 @@
 let num = [5,8,9,3]
 
-console.log(`Nosso valor é o ${num}`)
+// forma para acrescentar um elemento e valor a u a array ja sabendo o indice que eu quero colocar:
+num[3] = 6
+//forma mais pratica de adicionar elemento e valor a uma array sem sem preocupar com o indice:
+// (o numero que quero adc a array vai dentro dos parenteses no num.push)
+console.log(`O Primeiro elemento da array é ${num[0]}`)
+num.push(2)
+console.log(`Os elementos da array são ${num}`)
+// posso saber o comprimento de uma array é usando o atributo length
+//num.length 
+console.log(`Essa array tem ${num.length} posições`)
+//num.sort() coloca os elementos em ordem crescente
+num.sort()
