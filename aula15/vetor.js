05266f0fb18779e73b22d842ef508c5a983db71d
Helen <fer.helen@icloud.com>
2023-09-11 21:31:24 +0100

    simplificando for com for in

---
+++ aula15/vetor.js
@@ -0,0 +1,15 @@
+var valores = [8,1,7,4,2,9]
+
+//console,log(valores)
+/*
+console.log(0)
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+*/
+
+for(var pos=0; pos < valores.length; pos++) {
+    console.log(`O índice ${pos} tem o valor ${valores[pos]}`)
+}
