594ef416c88c3238d5235d8262c0dfb6f790f0f1
Helen <fer.helen@icloud.com>
2023-09-12 11:45:57 +0100

    comentario funcao recursiva

--- aula16/funcao05.js
+++ aula16/funcao05.js
@@ -10,7 +10,8 @@ function fatorial(n){
 
 //que é a mesma coisa que:
 
-// ! = indica que um numero é fatorial(n!) ou seja é a multiplicação de n por seus antecessores maiores ou iguais a 1.
+// ! = indica que um numero é fatorial(n!) 
+//ou seja é a multiplicação de n por seus antecessores maiores ou iguais a 1.
 /*
 5! = 5x 4x 3x 2x 1 = 120
 ou
