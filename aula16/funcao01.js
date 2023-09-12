2f1bbc1da2a41bc9604cdc3c6f33a5d8b42a8ac0
Helen <fer.helen@icloud.com>
2023-09-12 11:23:26 +0100

    funcao com parametro

---
+++ aula16/funcao01.js
@@ -0,0 +1,14 @@
+function parimp(n){
+    if(n%2 == 0){
+        return "par!"
+    } else {
+        return "impar!"
+    }
+}
+
+let res = parimp(4)
+console.log(res)
+
+//ou 
+
+console.log(parimp(9))
