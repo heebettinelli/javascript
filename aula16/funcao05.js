aef436a463ddac40c52c71134befc0abf808e5b8
Helen <fer.helen@icloud.com>
2023-09-12 11:44:40 +0100

    funcao recursiva

---
+++ aula16/funcao05.js
@@ -0,0 +1,20 @@
+// funcao RECURSIVA é quando uma funcao retorna ELA MESMA:
+
+function fatorial(n){
+    if(n ==1){
+        return  1
+    } else {
+        return n* fatorial(n - 1)
+    } 
+}
+
+//que é a mesma coisa que:
+
+// ! = indica que um numero é fatorial(n!) ou seja é a multiplicação de n por seus antecessores maiores ou iguais a 1.
+/*
+5! = 5x 4x 3x 2x 1 = 120
+ou
+5! = 5 x 4!
+n! = n x (n-1)!
+1! = 1
+*/
