f9a5b75664e6dbfe0093e161acc17d577e2ad15f
Helen <fer.helen@icloud.com>
2023-09-12 10:21:35 +0100

    changing while to for

--- aula15/vetor.js
+++ aula15/vetor.js
@@ -9,7 +9,12 @@ console.log(3)
 console.log(4)
 console.log(5)
 */
-
+/*
 for(var pos=0; pos < valores.length; pos++) {
     console.log(`O índice ${pos} tem o valor ${valores[pos]}`)
+}
+*/
+
+for(var pos in valores ){
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
