b1cca97afa632fc73a93435e7980495137ea29f4
Helen <fer.helen@icloud.com>
2023-09-12 11:29:36 +0100

    parametros e retornos js

---
+++ aula16/funcao02.js
@@ -0,0 +1,7 @@
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+console.log(soma(2,9))
+//considera o n1 primeiro + o n2 e assim me da o resultado da soma
+console.log(soma(3))
+//so retornou 3 pq eu coloquei no parametro 0 caso eu nao mencione algum valor de N, se eu nao tivesse colocado n2=0 iria aparecer nan no resultado pq eu declarei 2 parametros mas so chamei 1.
