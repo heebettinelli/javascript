0ea66aa798fe9e60d883b622a24e38c8c0ce8f0e
Helen <fer.helen@icloud.com>
2023-09-13 20:54:43 +0100

    js desafio 003

--- desafios/desafio003/script.js
+++ desafios/desafio003/script.js
@@ -1,1 +1,15 @@
-window.alert("tudo ok!")
+let num = document.querySelector("input#fnum")
+let lista = document.querySelector("select#flista")
+let res  = document.querySelector("div#res")
+let valores = []
+
+function adicionar(){
+    if (isNumero() ) {
+         return "Por favor verifique os dados!"
+    } else {
+        var adcNum = Number(num.value)
+        var resNum = Number(lista.value)
+
+
+    }
+}
