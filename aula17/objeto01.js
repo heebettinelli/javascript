5a4f0819b2dc808e7aa9f563489a8f7ce611fb01
Helen <fer.helen@icloud.com>
2023-09-18 11:49:15 +0100

    object in js

---
+++ aula17/objeto01.js
@@ -0,0 +1,9 @@
+let amigo = {nome: "jose", 
+sexo: "masculino", 
+peso: 80.2, 
+engordar(p = 0){
+    console.log("Engordou") 
+    this.peso += p
+}}
+amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso}kg`)
